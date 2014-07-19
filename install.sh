2657d643c5a9a061afb6cd1910821041640fe2f6
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-07-20 00:34:21 +0200

    Update install.sh to add symbolic link .editorconfig

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 ln -s ~/.vim/pathogen/autoload/pathogen.vim ~/.vim/autoload/
 ln -s ~/.vim/.vimrc ~/
 ln -s ~/.vim/.gvimrc ~/
-
+ln -s ~/.vim/.editorconfig ~/
 # install submodule
 git submodule init
 git submodule update
